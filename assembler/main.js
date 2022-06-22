eb1c5b004d04552d80336cdef834a0fc190e2a82
Wadedacoder <devm4966@gmail.com>
2022-06-22 15:57:21 +0530

    Init the project

---
+++ assembler/main.js
@@ -0,0 +1,29 @@
+function verifyTypeA (instruction) {
+  return -1
+}
+
+function verifyTypeB (instruction) {
+  return -1
+}
+
+<<<<<<< HEAD
+function verifyTypeC (array) {
+  return -1
+}
+
+function verifyTypeD (array) {
+  return -1
+}
+
+function verifyTypeE (instruction) {
+  return -1
+}
+
+function verifyTypeF (instruction) {
+  return -1
+}
+
+module.exports = { verifyTypeA, verifyTypeB, verifyTypeC, verifyTypeD, verifyTypeE, verifyTypeF }
+=======
+module.exports = { verifyTypeA, verifyTypeB }
+>>>>>>> b3d6e17 (Added test cases for Type A and Type B Opcode)
